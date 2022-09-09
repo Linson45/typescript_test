577d5bcdd22a9e8ab7e5b3b6a3fd76220a584be4
linson_kw <linson.lorance@kilowott.com>
2022-09-09 10:11:04 +0530

    new git

---
+++ src/config/config.ts
@@ -0,0 +1,13 @@
+const config = {
+    firebase: {
+        apiKey: "AIzaSyATlbVlKTsXe1uKfyc249ZCHPRNsZ49Dfc",
+        authDomain: "jensenn.firebaseapp.com",
+        projectId: "jensenn",
+        storageBucket: "jensenn.appspot.com",
+        messagingSenderId: "407442587427",
+        appId: "1:407442587427:web:aa40d4eb914398f5dc9696",
+        measurementId: "G-GZDH77SG71"
+    }
+}
+
+export default config;
